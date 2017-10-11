8114d46c82acf4d5528d724c54bd7c0153721ce8
Markus Ziegler <mrksziegler@gmail.com>
2017-10-11 19:14:47 +0200

    PCA fertiggestellt

--- calcPrincipalComponents.m
+++ calcPrincipalComponents.m
@@ -1,29 +1,33 @@
-function [ outData, EVe ] = calcPrincipalComponents( data, dimReduct )
+function [ outData, EVe ] = calcPrincipalComponents( data, dimReduct, coeffs )
 
-    % From the Pattern Recognition Scriptum
+    % From https://faculty.iiit.ac.in/~mkrishna/PrincipalComponents.pdf        
+    % And from the Pattern Recognition Scriptum
     
     % Subtract the mean in each coordinate
     % here we use the outer product to do this in an elegant way
     dataCentered = data.x - ones(size(data.x, 1), 1) * mean(data.x, 1);
 
-    % Calculate the covariance matrix
-    Sigma = cov(dataCentered);
+    if nargin == 2
+        % Calculate the covariance matrix
+        Sigma = cov(dataCentered);
     
-    % Compute eigenvalues and eigenvectors
-    [EVe, EVa] = eig(Sigma);
+        % Compute eigenvalues and eigenvectors
+        [EVe, EVa] = eig(Sigma);
     
-    % We take out the eigenvalues from the diagonal matrix and sort them
-    % idx contains the sorting index which we need to sort
-    % the eigenvectors in the same order
-    [EVa, idx] = sort(diag(EVa),'descend');
+        % We take out the eigenvalues from the diagonal matrix and sort them
+        % idx contains the sorting index which we need to sort
+        % the eigenvectors in the same order
+        [EVa, idx] = sort(diag(EVa),'descend');
     
-    % Lets resort the eigenvectors
-    EVe = EVe(:,idx);
-    
+        % Lets resort the eigenvectors
+        EVe = EVe(:,idx);
+        coeffs = EVe;
+    end
     % Now transform the data to the new coordinates by multiplying
     % (this works since the eigenvectors form an orthonormal basis so
     % that coordinates can simply be calculated by the inner product)
-    outData.x = (EVe'*dataCentered')';
+    transformedData = (coeffs'*dataCentered')';
+    outData.x = transformedData(:,1:dimReduct);
     outData.targets = data.targets;
     outData.labels = data.labels;
 end
